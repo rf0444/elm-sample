b8478e866b5b14db25f92ad86be1ae82b77ec0f8
rf <rf0444@gmail.com>
2015-07-20 18:18:34 +0900

    use style

--- elm/src/View/Counter.elm
+++ elm/src/View/Counter.elm
@@ -3,15 +3,31 @@ module View.Counter
   ) where
 
 import Signal exposing (Address)
-import Html exposing (Html, div, button, text)
+
+import Html exposing (Html, button, div, span, text)
+import Html.Attributes exposing (class, style)
 import Html.Events exposing (onClick)
+
 import Model.Counter as M
 import Update.Counter as U
 
 view : Address U.Action -> M.Model -> Html
 view address model =
-  div []
-    [ button [ onClick address U.Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick address U.Increment ] [ text "+" ]
+  div
+    [ style [ ("text-align", "center") ] ]
+    [ button
+      [ class "btn btn-default"
+      , onClick address U.Decrement 
+      ]
+      [ text "-" ]
+    , span
+      [ style
+        [ ("display", "inline-block")
+        , ("width", "40px")
+        ]
+      ]
+      [ text (toString model) ]
+    , button
+      [ class "btn btn-default"
+      , onClick address U.Increment ] [ text "+" ]
     ]
